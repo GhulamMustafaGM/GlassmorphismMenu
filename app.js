1d9dc7d5349b37d57069a9f5091c95f9cccf87fa
Ghulam Mustafa <mustafaji@gmail.com>
2021-04-17 19:51:13 +0200

    completed Glassmorphism Menu App

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const marker = document.querySelector
     ('#marker');
-const item = document.querySelector('ul li a');
+const item = document.querySelectorAll('ul li a');
 
 function Indicator(e) {
-    marker.getElementsByClassName.left = e.offsetLeft + 'px';
-    marker.getElementsByClassName.width = e.offsetWidth + 'px';
+    marker.style.left = e.offsetLeft + 'px';
+    marker.style.width = e.offsetWidth + 'px';
 }
+
+item.forEach(Link => {
+    Link.addEventListener('mousemove', (e) => {
+        Indicator(e.target);
+    })
+})
